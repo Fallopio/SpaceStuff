5d8557f73e940583c81fd57f4373a451fc38fe17
Fallopio <kibaryshnikov@yandex.ru>
2020-05-05 19:25:51 +0400

    Updates

---
+++ precache-manifest.ff0b683ff2904d1851c3b9aaba6ed6f2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6524c63fac3d57c781130c47f0019bc2",
+    "url": "/SpaceStuff/index.html"
+  },
+  {
+    "revision": "d8d188d2da2d01369829",
+    "url": "/SpaceStuff/static/css/2.7fbb691e.chunk.css"
+  },
+  {
+    "revision": "5895b607d1986297352f",
+    "url": "/SpaceStuff/static/css/main.869f8fd5.chunk.css"
+  },
+  {
+    "revision": "d8d188d2da2d01369829",
+    "url": "/SpaceStuff/static/js/2.a3c45c61.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/SpaceStuff/static/js/2.a3c45c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5895b607d1986297352f",
+    "url": "/SpaceStuff/static/js/main.8664c618.chunk.js"
+  },
+  {
+    "revision": "18b8cecff5e2dbf2ed6c",
+    "url": "/SpaceStuff/static/js/runtime-main.f3675f93.js"
+  },
+  {
+    "revision": "2be3cf9428a7a0d41ead8a50fbd7d311",
+    "url": "/SpaceStuff/static/media/earth.2be3cf94.jpg"
+  },
+  {
+    "revision": "56ac37987396a699e3a67c7765198a57",
+    "url": "/SpaceStuff/static/media/jupiter.56ac3798.jpg"
+  },
+  {
+    "revision": "32c92c0314ef5aff4e698c13eaf80f47",
+    "url": "/SpaceStuff/static/media/mars.32c92c03.jpg"
+  },
+  {
+    "revision": "d60e031acc197812d1abc719ead0d5f6",
+    "url": "/SpaceStuff/static/media/moon.d60e031a.jpg"
+  },
+  {
+    "revision": "bacdce9d59b970833e12c98bcb8128f8",
+    "url": "/SpaceStuff/static/media/saturn.bacdce9d.jpg"
+  },
+  {
+    "revision": "a879f36d1281f772aca14fea5ab89bae",
+    "url": "/SpaceStuff/static/media/stars.a879f36d.jpg"
+  },
+  {
+    "revision": "3e256a6a46acc8c8528c9c515c6830c2",
+    "url": "/SpaceStuff/static/media/sun_background.3e256a6a.jpg"
+  },
+  {
+    "revision": "24328abc7198df77326900894621b2ee",
+    "url": "/SpaceStuff/static/media/telescope.24328abc.jpg"
+  },
+  {
+    "revision": "c86b1c2dfbac10494e576ce99f1f101f",
+    "url": "/SpaceStuff/static/media/venus.c86b1c2d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca715a7bb97b99f9e94bcabd384be510.js"
+  "/SpaceStuff/precache-manifest.ff0b683ff2904d1851c3b9aaba6ed6f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SpaceStuff/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
