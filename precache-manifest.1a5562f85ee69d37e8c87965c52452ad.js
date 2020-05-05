06b5528ffbc415ce963e968fdc62271015db9382
Fallopio <kibaryshnikov@yandex.ru>
2020-05-05 18:59:54 +0400

    Updates

---
+++ precache-manifest.1a5562f85ee69d37e8c87965c52452ad.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281f592c471f7343cccedddb4034ee65",
+    "url": "/SpaceStuff/index.html"
+  },
+  {
+    "revision": "3d5dd751e9ccfb8fd995",
+    "url": "/SpaceStuff/static/css/2.7fbb691e.chunk.css"
+  },
+  {
+    "revision": "068c64f94b84c4cd0d77",
+    "url": "/SpaceStuff/static/css/main.869f8fd5.chunk.css"
+  },
+  {
+    "revision": "3d5dd751e9ccfb8fd995",
+    "url": "/SpaceStuff/static/js/2.222404f1.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/SpaceStuff/static/js/2.222404f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068c64f94b84c4cd0d77",
+    "url": "/SpaceStuff/static/js/main.2d85a742.chunk.js"
+  },
+  {
+    "revision": "18b8cecff5e2dbf2ed6c",
+    "url": "/SpaceStuff/static/js/runtime-main.f3675f93.js"
+  },
+  {
+    "revision": "a879f36d1281f772aca14fea5ab89bae",
+    "url": "/SpaceStuff/static/media/stars.a879f36d.jpg"
+  },
+  {
+    "revision": "3e256a6a46acc8c8528c9c515c6830c2",
+    "url": "/SpaceStuff/static/media/sun_background.3e256a6a.jpg"
+  },
+  {
+    "revision": "24328abc7198df77326900894621b2ee",
+    "url": "/SpaceStuff/static/media/telescope.24328abc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca715a7bb97b99f9e94bcabd384be510.js"
+  "/SpaceStuff/precache-manifest.1a5562f85ee69d37e8c87965c52452ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SpaceStuff/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
