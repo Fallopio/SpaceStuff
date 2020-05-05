546e8ab8f88954f0f1c3cbe01f713e54772637e5
Fallopio <kibaryshnikov@yandex.ru>
2020-05-05 18:59:12 +0400

    Updates

---
+++ precache-manifest.ca715a7bb97b99f9e94bcabd384be510.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a87ce6ae17b5cfd833a0e7538d9aa26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d5dd751e9ccfb8fd995",
+    "url": "/static/css/2.7fbb691e.chunk.css"
+  },
+  {
+    "revision": "62256fdeba756a0a69c8",
+    "url": "/static/css/main.de815d4c.chunk.css"
+  },
+  {
+    "revision": "3d5dd751e9ccfb8fd995",
+    "url": "/static/js/2.222404f1.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/static/js/2.222404f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62256fdeba756a0a69c8",
+    "url": "/static/js/main.27112b69.chunk.js"
+  },
+  {
+    "revision": "f1b7d199f5ebbc0855ea",
+    "url": "/static/js/runtime-main.c840414b.js"
+  },
+  {
+    "revision": "a879f36d1281f772aca14fea5ab89bae",
+    "url": "/static/media/stars.a879f36d.jpg"
+  },
+  {
+    "revision": "3e256a6a46acc8c8528c9c515c6830c2",
+    "url": "/static/media/sun_background.3e256a6a.jpg"
+  },
+  {
+    "revision": "24328abc7198df77326900894621b2ee",
+    "url": "/static/media/telescope.24328abc.jpg"
+  }
+]);
