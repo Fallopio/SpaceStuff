a75f50ffeff76f6bc9b083e6144c6d6cd5cc3753
Fallopio <kibaryshnikov@yandex.ru>
2020-05-05 19:28:52 +0400

    className fix

--- src/Components/mars-carousel/marsCarousel.jsx
+++ src/Components/mars-carousel/marsCarousel.jsx
@@ -14,7 +14,7 @@ function MarsCarousel(props) {
             props.marsPics.length > 1 ?
             props.marsPics.map(i =>
                 <div key={i.id}>
-                    <img className='s' src={i.img_src} alt="" />
+                    <img className='marsPhoto' src={i.img_src} alt="" />
                 </div>
             ): <div />}
         </Carousel>
