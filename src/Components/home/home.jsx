0d7620a2a936dbffd734ac1e093c67ea36f82ab7
Fallopio <kibaryshnikov@yandex.ru>
2020-06-12 19:24:51 +0400

    added speed range

--- src/Components/home/home.jsx
+++ src/Components/home/home.jsx
@@ -10,38 +10,41 @@ import Jup from '../../assets/planet-icons/jupiter.png'
 import Sat from '../../assets/planet-icons/saturn.png'
 import Uranus from '../../assets/planet-icons/uranus.png'
 import Nep from '../../assets/planet-icons/neptune.png'
+import { useState } from 'react'
 
 function Home() {
+    const [speed, setSpeed] = useState(1)
     return (
         <div className='home'>
             <div className='system'>
                 <Link to='/SpaceStuff/sun'>
                     <img style={{ zIndex: '13' }} className='sun' src={Sun} alt="" />
                 </Link>
-                <div className='mercury'>
+                <div className='mercury' style={{animation: `rotate ${10 / speed}s infinite linear`}}>
                     <img className='planetImg' src={Merc} alt="" />
                 </div>
-                <div className='venus'>
+                <div className='venus' style={{animation: `rotate ${12 / speed}s infinite linear`}}>
                     <img className='planetImg' src={Venus} alt="" />
                 </div>
-                <div className='earth'>
+                <div className='earth' style={{animation: `rotate ${14 / speed}s infinite linear`}}>
                     <img className='planetImg' src={Earth} alt="" />
                 </div>
-                <div className='mars'>
+                <div className='mars' style={{animation: `rotate ${16 / speed}s infinite linear`}}>
                     <img className='planetImg' src={Mars} alt="" />
                 </div>
-                <div className='jupiter'>
+                <div className='jupiter' style={{animation: `rotate ${18 / speed}s infinite linear`}}>
                     <img className='planetImg' src={Jup} alt="" />
                 </div>
-                <div className='saturn'>
+                <div className='saturn' style={{animation: `rotate ${20 / speed}s infinite linear`}}>
                     <img className='planetImg' src={Sat} alt="" />
                 </div>
-                <div className='uranus'>
+                <div className='uranus' style={{animation: `rotate ${22 / speed}s infinite linear`}}>
                     <img className='planetImg' src={Uranus} alt="" />
                 </div>
-                <div className='neptune'>
+                <div className='neptune' style={{animation: `rotate ${24 / speed}s infinite linear`}}>
                     <img className='planetImg' src={Nep} alt="" />
                 </div>
+                <input type="range" min='1' max='10' defaultValue='1' step='0.1' onChange={e => setSpeed(e.target.value)}/>
             </div>
         </div>
     )
